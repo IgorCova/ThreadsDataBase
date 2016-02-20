174431f5fb772480150705655ac7e949bdd5eb34
Igor Cova <emptyparam@gmail.com>
2016-02-21 01:45:12 +0300

    Change logic show date info for CreateDate Entries

--- System/Function/fn.datetime_to_text_ForUser.sql
+++ System/Function/fn.datetime_to_text_ForUser.sql
@@ -21,38 +21,38 @@ begin
 -- v1.0: Created by Cova Igor 02.02.2016
 ----------------------------------------------
   declare 
-     @years  int
-    ,@months int 
-    ,@weeks  int 
-    ,@days   int 
-    ,@hours  int
-    ,@mins   int 
-    ,@sec    int
+     @year int
+    ,@mont int 
+    ,@week int 
+    ,@days int 
+    ,@hour int
+    ,@mins int 
+    ,@secs int
 
-  set @years = nullif(datediff(year, @date, getdate()), 0)
-
-  if @years is null
+  if nullif(datediff(month, @date, getdate()), 0) > 12
+    set @year = nullif(datediff(year, @date, getdate()), 0)
+  else if @year is null
   begin
-    set @months = nullif(datediff(month, @date, getdate()),0)
+    set @mont = nullif(datediff(month, @date, getdate()),0)
 
-    if @months is null
+    if @mont is null
     begin
-      set @weeks = nullif(datediff(week, @date, getdate()), 0)
-
-      if @weeks is null
+      if (nullif(datediff(day, @date, getdate()), 0) % 7) = 0
+        set @week = nullif(datediff(week, @date, getdate()), 0)
+      else 
       begin
         set @days = nullif(datediff(day, @date, getdate()), 0)
 
         if @days is null
         begin
-          set @hours = nullif(datediff(hour, @date, getdate()), 0)
+          set @hour = nullif(datediff(hour, @date, getdate()), 0)
 
-          if @hours is null
+          if @hour is null
           begin
             set @mins = nullif(datediff(minute, @date, getdate()), 0)
 
             if @mins is null
-              set @sec = datediff(second, @date, getdate())
+              set @secs = datediff(second, @date, getdate())
           end
         end
       end
@@ -60,13 +60,13 @@ begin
   end
 
   return ( 
-    concat(try_cast(@years as varchar(4)) + ' years ago'
-          ,try_cast(@months as varchar(8)) + ' month ago'
-          ,try_cast(@weeks as varchar(8)) + ' weeks ago'
-          ,try_cast(@days as varchar(8)) + ' days ago'
-          ,try_cast(@hours as varchar(8)) + ' hours ago'
-          ,try_cast(@mins as varchar(8)) + ' mins ago'
-          ,iif(@sec >= 0 and @sec <60, 'just now', '')))
+    concat(iif(@year = 1, '1 year',  try_cast(@year as varchar(4)) + ' years')
+          ,iif(@mont = 1, '1 month', try_cast(@mont as varchar(8)) + ' months')
+          ,iif(@week = 1, '1 week',  try_cast(@week as varchar(8)) + ' weeks')
+          ,iif(@days = 1, '1 day',   try_cast(@days as varchar(8)) + ' days')
+          ,iif(@hour = 1, '1 hour',  try_cast(@hour as varchar(8)) + ' hours')
+          ,iif(@mins = 1, '1 min',   try_cast(@mins as varchar(8)) + ' mins')
+          ,iif(@secs >= 0 and @secs < 60, 'just now', '')))
 end
-go 
+go
 grant execute on fn.datetime_to_text_ForUser to [public]
